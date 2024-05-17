1cc1cd60b3082a45cdd1e284c28e1befe1043cbf
Mihir Thakur <tamihir27@yahoo.com>
2024-05-16 19:13:34 -0700

    Updated Skills section

--- src/components/skill-card/skillCard.js
+++ src/components/skill-card/skillCard.js
@@ -1,18 +1,15 @@
 import "./skillCard.css";
 
-export default function SkillCard({ logos, title, description }) {
-	return (
-		<div className="card">
-			{logos ? (
-				<div className="card-logos">
-					{console.log(logos)}
-					{logos.map((logo) => {
-						return <i key={logo} className={logo + " logo"}></i>;
-					})}
-				</div>
-			) : null}
-			<div className="card-title">{title}</div>
-			<div className="card-description">{description}</div>
-		</div>
-	);
+export default function SkillCard({ logo, skill }) {
+    return (
+        <div className="card">
+            {logo ? (
+                <div className="card-logo">
+                    <img src={logo} alt={skill} />
+                </div>
+            ) : null}
+            <div className="card-title">{skill}</div>
+            {/* <div className="card-description">{description}</div> */}
+        </div>
+    );
 }
--- src/container/skillsContainer/skills.js
+++ src/container/skillsContainer/skills.js
@@ -1,6 +1,7 @@
 // import skillsData from "../../skills.json";
 import SkillCard from "../../components/skill-card/skillCard";
 import "./skills.css";
+import skillsData from "../../assets/data/skillsData.json";
 
 export default function Skills() {
     return (
@@ -9,36 +10,24 @@ export default function Skills() {
                 <h1>SKILLS</h1>
             </div>
             <div className="skills-info">
-                <SkillCard
-                    logos={["fab fa-html5", "fab fa-css3-alt"]}
-                    title="HTML, CSS"
-                    description="The basics of web development. Familiar with latest semantic elements of HTML and pretty used to building responsive stylings in CSS."
-                />
-                <SkillCard
-                    logos={["fab fa-node-js"]}
-                    title="Node JS"
-                    description="Node JS is the first backend that I used with Express engine and Restful Routing. Implemented middleware for user authentication. Established connection with MongoDB using Mongoose."
-                />
-                <SkillCard
-                    logos={["fab fa-react"]}
-                    title="React JS"
-                    description="Built projects using React. Extensively use functions with React Hooks in React rather than classes. Even this website is built using React."
-                />
-                <SkillCard
-                    logos={["fas fa-database"]}
-                    title="MySQL, MongoDB, Firebase"
-                    description="Implemented MySQL connectivity for offline projects(aaplications). Querying various fields through the language used to the database. Used MongoDB and Firebase with web applications and pretty familiar with their structure and working."
-                />
-                <SkillCard
-                    logos={["fab fa-python"]}
-                    title="Python"
-                    description="Python is a very intriguing and easy to code programming language. Have used python for small competitions and challenges. Accustomed to the libraries related to data science."
-                />
-                <SkillCard
-                    logos={["fas fa-sitemap"]}
-                    title="Machine Learning"
-                    description="I have implemented machine learning techniques extensively in Python. Have done mini-courses in data manipulation and visualization using Pandas and Matplotlib/Seaborn. Currently enrolled in a course on Neural Network on Coursera."
-                />
+                {Object.keys(skillsData).map((d) => {
+                    return (
+                        <div className="skills-category">
+                            <h2>{d}</h2>
+                            <div className="skills-category-info">
+                                {skillsData[d].map((skill) => {
+                                    return (
+                                        <SkillCard
+                                            skill={skill.skill}
+                                            logo={require("../../assets/skills/" +
+                                                skill.logo)}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
